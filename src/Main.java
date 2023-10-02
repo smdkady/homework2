73fc4828ddb2eea4b69e52b0f2daf2de7ef3f3f0
vexed <kadirthekid@gmail.com>
2023-10-02 19:10:01 +0300

    changed src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int startingBalance = 1000;
+        int refill = 2100;
+        int bonus = 0;
+
+        if (refill > 1000) {
+        bonus = refill / 100;
+        }
+        int finalBalance = startingBalance + refill + bonus;
+        System.out.println("Баланс " + finalBalance);
+        System.out.println("Получен(о) бонус(ов) " + bonus);
+
+    }
+}
